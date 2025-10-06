de056f68956d9136f1f0a6611aab3fce826887a8
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-10-06 21:47:54 +0200

    Navbar component created and index.js file with the navlinks

---
+++ Shiynelly Lashes/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+	{ name: "Home", path: "/" },
+	{ name: "Nos Prestations", path: "#nos prestations" },
+	{ name: "Contact", path: "#contact" },
+	{ name: "Réservation", path: "#réservation" },
+];
+
